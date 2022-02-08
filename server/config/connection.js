7b95317bbc4b0dc17a61f78ca5ecc064d68d8c7b
aorich <rich.aidan@gmail.com>
2022-02-08 15:36:19 -0500

    database name change, begin social refactor

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/aidanart',
+  process.env.MONGODB_URI || 'mongodb://localhost/illust',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
